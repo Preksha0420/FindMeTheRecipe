78ad53a2c3377ce46cd200807f1a66a78bdda3a4
Preksha Kamleshkumar Shah <113962330+Preksha0420@users.noreply.github.com>
2023-11-16 14:01:54 -0500

    Updating as Spoonacular API changed some parameters

--- FindMeTheRecipe.py
+++ FindMeTheRecipe.py
@@ -1,6 +1,15 @@
 import requests
 import json
+# importing modules
+# import urllib.request
+# from PIL import Image
 
+# urllib.request.urlretrieve(
+#   'https://media.geeksforgeeks.org/wp-content/uploads/20210318103632/gfg-300x300.png',
+#    "gfg.png")
+  
+# img = Image.open("gfg.png")
+# img.show()
 API_key = "0164c34b839d44d49c73e9ac07c4c0dc"
 
 class Recipe:
@@ -14,34 +23,9 @@ class Recipe:
         self.carbs = carbs
         self.fiber = fiber
         
-# cuisine_tree = ("Would you like to explore our cuisines?", ("Would you like to have Indian food? ", ("Indian", None, None), ("Italian", None, None)), ("No worries, we will consider every cuisine! (Enter \"yes\" to continue)", None, None))
+cuisine_tree = ("Would you like to explore our cuisines?", ("Would you like to have Indian food? ", ("Indian", None, None), ("Italian", None, None)), ("No worries, we will consider every cuisine! (Enter \"yes\" to continue)", None, None))
 
 def tree_c(tree):
-    """
-    this function accepts a single argument, which is a tree, and
-    plays the game once by using the tree to guide its questions. The function
-    returns the user selected cuisine and a new tree that is the result of 
-    playing the game on the  original tree and learning from the answers.
-
-    Parameters
-    ----------
-    tree : A tuple of tuples ( a tree )
-        A tuple of three tuples containing the following in order:
-            1. A question.
-            2. Response to the "YES" answer of the question.
-            3. Response to the "NO" answer to the question.
-
-    Returns
-    -------
-    cuisine: A string
-        A string stating the cuisine the user would like to eat.
-    tree : A tuple of tuples ( a tree )
-        A tuple of three tuples containing the following in order:
-            1. A question.
-            2. Response to the "YES" answer of the question.
-            3. Response to the "NO" answer to the question.
-
-    """
     if tree[1] == None and tree[2] == None:
         # It is a leaf
         inp = input(tree[0] + ": ")
@@ -50,7 +34,7 @@ def tree_c(tree):
             cuisine = tree[0]
             if cuisine == "No worries, we will consider every cuisine! (Enter \"yes\" to continue)":
                 cuisine = None
-            newTree = tree
+            return cuisine, tree
         else:
             q1 = input("Drats! What cuisine would you like to eat? : ")
             # sen = "Would you like to have " + q1 + " food? " 
@@ -61,7 +45,7 @@ def tree_c(tree):
             elif ans.lower() == "no":
                 newTree =  (sen, tree, (q1, None, None))
             cuisine = q1
-        return cuisine, newTree
+            return cuisine, newTree
     else:
         inp = input(tree[0] + " : ")
         if inp.lower() == "yes":    
@@ -69,48 +53,24 @@ def tree_c(tree):
               tt = list(tree)
               tt[1] = t
               tree = tuple(tt)
+              return c, tree
         else:
               c, t = tree_c(tree[2])
               tt = list(tree)
               tt[2] = t
               tree = tuple(tt)
-        return c, tree
+              return c, tree
         
-# diet_tree = ("Would you like to explore our dietary options?", ("Would you like to have Vegetarian? ", ("Vegetarian", None, None), ("Chicken", None, None)), ("No worries, we will consider all diet options! (Enter \"yes\" to continue)", None, None))
+diet_tree = ("Would you like to explore our dietary options?", ("Would you like to have Vegetarian? ", ("Vegetarian", None, None), ("Chicken", None, None)), ("No worries, we will consider all diet options! (Enter \"yes\" to continue)", None, None))
 
 def tree_d(tree):
-    """
-    this function accepts a single argument, which is a tree, and
-    plays the game once by using the tree to guide its questions. The function
-    returns the user selected diet and a new tree that is the result of 
-    playing the game on the  original tree and learning from the answers.
-
-    Parameters
-    ----------
-    tree : A tuple of tuples ( a tree )
-        A tuple of three tuples containing the following in order:
-            1. A question.
-            2. Response to the "YES" answer of the question.
-            3. Response to the "NO" answer to the question.
-
-    Returns
-    -------
-    diet: A string
-        A string stating the diet the user would like to eat.
-    tree : A tuple of tuples ( a tree )
-        A tuple of three tuples containing the following in order:
-            1. A question.
-            2. Response to the "YES" answer of the question.
-            3. Response to the "NO" answer to the question.
-
-    """
     if tree[1] == None and tree[2] == None:
         inp = input(tree[0] + ": ")
         if inp.lower() == "yes":
             diet = tree[0]
             if diet == "No worries, we will consider all diet options! (Enter \"yes\" to continue)":
                 diet = None
-            newTree = tree
+            return diet, tree
         else:
             q1 = input("Drats! What's your dietary preference? (e.g. Chicken): ")
             sen = input("What differentiates between " + q1 + " and " + tree[0] + "? :")
@@ -120,7 +80,7 @@ def tree_d(tree):
             elif ans.lower() == "no":
                 newTree =  (sen, tree, (q1, None, None))
             diet = q1
-        return diet, newTree
+            return diet, newTree
     else:
         inp = input(tree[0] + " : ")
         if inp.lower() == "yes":    
@@ -128,12 +88,13 @@ def tree_d(tree):
               tt = list(tree)
               tt[1] = t
               tree = tuple(tt)
+              return c, tree
         else:
               c, t = tree_d(tree[2])
               tt = list(tree)
               tt[2] = t
               tree = tuple(tt)
-        return c, tree
+              return c, tree
            
 def saveTree(tree, treeFile):
     """
@@ -175,8 +136,7 @@ def loadTree(treeFile):
     Parameters
     ----------
     treeFile : file
-        A file handle that is open for reading. The tree will be read and 
-        loaded from this file.
+        A file handle that is open for reading. The tree will be read and loaded from this file.
         
     Returns
     -------
@@ -239,36 +199,9 @@ def save_cache(cache_dict):
     fw.write(dumped_json_cache)
     fw.close()
 
-def get_recipe_name(query, cuisine=None, diet=None, sort="calories", sortDirection="asc", number=10, limitLicense=True):
-    """
-    Calls the API and gets top suggeted recipes based on the input parameters
-
-    Parameters
-    ----------
-    query : string
-        This is the recipe search string that the user inputs to the program.
-    cuisine : string, optional
-        The user selected cuisine (from the tree). The default is None.
-    diet : string, optional
-        The user selected diet (from the tree). The default is None.
-    sort : string, optional
-        Sorts the recipe suggetions based on this. The default is "calories".
-    sortDirection : string, optional
-        "asc" or "dsc" to sort the recipies in ascending or descending. The default is "asc".
-    number : int, optional
-        Maximum number of suggested recipies. The default is 10.
-    limitLicense : boolean, optional
-        Whether the recipes should have an open license that allows display 
-        with proper attribution.. The default is True.
-
-    Returns
-    -------
-    recipes : dictionary
-        A dictionary of suggested recipies.
-
-    """
+def get_recipe_name(query, cuisine=None, excludeCuisine=None, diet=None, intolerance=None, equipment=None, includeIngredients=None, excludeIngredients=None, instructionsRequired=False, fillIngredients=False, addRecipeInformation=False, addRecipeNutrition=False, author=None, tags=None, recipeBoxId=0, maxReadyTime=100, ignorePantry=False, sort="calories", sortDirection="asc", minCarbs=0, minProtein=0, maxProtein=100, minCalories=0, minFat=0, maxFat=100, minAlcohol=0, minFiber=0, maxFiber=100, number=10, limitLicense=True):
     baseurl = "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + API_key
-    parameters = {"query": query, "cusine": cuisine, "diet": diet, "sort": sort, "sortDirection": sortDirection, "number": number, "limitLicense": True}
+    parameters = {"query": query, "cusine": cuisine, "diet": diet, "intolerance": intolerance, "equipment": equipment, "includeIngredients": includeIngredients, "excludeIngredients": excludeIngredients, "instructionsRequired": instructionsRequired, "fillIngredients": fillIngredients, "addRecipeInformation": addRecipeInformation, "addRecipeNutrition": addRecipeNutrition, "author": author, "tags": tags, "recipeBoxID": recipeBoxId, "maxReadyTime": maxReadyTime, "ignorePantry": ignorePantry, "sort": sort, "sortDirection": sortDirection, "minCarbs": minCarbs, "minProtein": minProtein, "maxProtein": maxProtein, "minCalories": minCalories, "minFat": minFat, "maxFat": maxFat, "minFiber": minFiber, "maxFiber": maxFiber, "number": number, "limitLicense": True}
     response = requests.get(baseurl, parameters)
     recipes = json.loads(response.text)
     if recipes["totalResults"] == 0:
@@ -276,23 +209,12 @@ def get_recipe_name(query, cuisine=None, diet=None, sort="calories", sortDirecti
     return recipes
 
 def print_recipe(recipes):
-    """
-    Print the suggested recipes in a user friendly format. Displays all nutritional information as well
-    Parameters
-    ----------
-    recipes : list of objects of the Recipe class
-        A list containing different suggested recipies stored as objects of the Recipe class.
-
-    Returns
-    -------
-    None.
-
-    """
     i = 1
     for recipe in recipes:
         print("-----------------------------------------------------------")
         print("Recipe: ", i)
         print("Title: " + recipe.title)
+        # print(recipe.image)
         print("Calories: "+ recipe.calories + " kcal")
         print("Protein: "+ recipe.protein + " g")
         print("Fats: "+ recipe.fat + " g")
@@ -302,36 +224,9 @@ def print_recipe(recipes):
         print("-----------------------------------------------------------")
 
 def from_dict(d):
-    """
-
-    Parameters
-    ----------
-    d : dictionary
-        A dictionary version of a recipe from the Recipe class.
-
-    Returns
-    -------
-    Recipe object
-        An object of the recipe class with attributes filled from the values 
-        in the corresponding columns of the dicitonary d.
-
-    """
     return Recipe(d["i_d"], d["title"], d["image"], d["calories"], d["protein"], d["fat"], d["carbs"], d["fiber"])
 
 def print_steps(inst):
-    """
-    This function displays the recipe steps in a user friendly and numbered manner.
-
-    Parameters
-    ----------
-    inst : dictionary
-        A dictionary of the analyzed instructions of a particular recipe.
-
-    Returns
-    -------
-    None.
-
-    """
     i=0
     print("Recipe Steps: ")
     for num in inst[0]["steps"]:
@@ -340,21 +235,6 @@ def print_steps(inst):
     return
 
 def get_ingredients(inst):
-    """
-    This function extracts the ingredients from the instructions dictionary and 
-    outputs them as a list 
-
-    Parameters
-    ----------
-    inst : dictionary
-        A dictionary of the analyzed instructions of a particular recipe.
-
-    Returns
-    -------
-    ing: list
-        A list of unique ingredients used in the recipe    
-    """
-    
     ing = []
     for num in inst[0]["steps"]:
         for it in num["ingredients"]:
@@ -363,24 +243,6 @@ def get_ingredients(inst):
     return ing
     
 def play_tree(ing, i):
-    """
-    This function runs a tree with the ingredients for the user to check if 
-    they have the necessary ingredients or not
-
-    Parameters
-    ----------
-    ing : list
-        A list of unique ingredients used in the recipe 
-    
-    i: int
-        An iterative index to iterate in the ing list
-
-    Returns
-    -------
-    boolean
-        Return True if the user has all the ingredients
-        Return False if the user is missing any of the ingredients.
-    """
     q1 = input("Do you have " + ing[i] + "? : ")
     if q1.lower() == "yes":
         if ing[i] == ing[-1]:
@@ -392,16 +254,6 @@ def play_tree(ing, i):
         return False
      
 def get_cuisine():
-    """
-    A function that opens the cuisine.txt file, runs the tree and saves the 
-    updated tree in the same cuisine.txt file
-
-    Returns
-    -------
-    cuisine : string
-        A string stating the cuisine the user would like to eat.
-
-    """
     f_c = open("cuisine.txt", "r")
     cuisine_tree = loadTree(f_c)
     f_c.close()
@@ -414,16 +266,6 @@ def get_cuisine():
     return cuisine
 
 def get_diet():
-    """
-    A function that opens the diet.txt file, runs the tree and saves the 
-    updated tree in the same diet.txt file    
-
-    Returns
-    -------
-    diet : string
-        A string stating the diet the user would like to eat.
-
-    """
     f_d = open("diet.txt", "r")
     diet_tree = loadTree(f_d)
     f_d.close()
@@ -438,6 +280,7 @@ def get_diet():
 if __name__ == "__main__":
     cuisine = get_cuisine()
 
+    print(cuisine)
     if cuisine == None:
         ci = " "
     else:
@@ -450,11 +293,11 @@ if __name__ == "__main__":
 
     RECIPE_CACHE = open_cache()
 
-    q = input("Enter a recipe you would like to search: ")
+    q = input("Enter an ingridient you have in mind: ")
     if str(q.lower() + ci.lower() + di.lower()) in RECIPE_CACHE:
         recipes = [from_dict(d) for d in RECIPE_CACHE[str(q.lower() + ci.lower() + di.lower())]]
     else:
-        rec = get_recipe_name(q, cuisine=cuisine, diet=diet)
+        rec = get_recipe_name(q, cuisine=None, excludeCuisine=None, diet=None)
         i_ds = [results["id"] for results in rec["results"]]
         titles = [results["title"] for results in rec["results"]]
         cals = [str(results["nutrition"]["nutrients"][0]["amount"]) for results in rec["results"]]
@@ -496,4 +339,3 @@ if __name__ == "__main__":
                 print_steps(inst)
             else:
                 print("Please check another recipe, thank you!")
-
